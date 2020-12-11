0d43b061426e91a42001f954bc70c74a63add865
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-11 13:41:31 +0800

    Add error handler middleware

---
+++ middleware/error.js
@@ -0,0 +1,8 @@
+const errorhandler = (err, req, res, next) => {
+	// Log to console for developer
+	console.log(err.stack.red);
+
+	res.status(500).json({ success: false, error: err.message });
+};
+
+module.exports = errorhandler;
