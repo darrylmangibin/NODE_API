7088a0ee06eea15852a6ea0dba32bfe3698bb9cc
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-11 12:35:53 +0800

    Add basic functionality for creating a bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc    Get all bootcamps
 // @route   GET /api/vi/bootcamps
 // @access  Public
@@ -15,8 +17,14 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create new bootcamp
 // @route   POST /api/vi/bootcamps/
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-	res.status(200).json({ success: true, msg: "Create new bootcamps" });
+exports.createBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.create(req.body);
+
+		res.status(201).json({ success: true, data: bootcamp });
+	} catch (e) {
+		res.status(400).json({ success: false });
+	}
 };
 
 // @desc    Update bootcamp
