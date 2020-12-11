dd164dd7c5e06f91cae1e0e4048ea9f2112008d6
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-11 14:17:20 +0800

    Add error message for validation

--- middleware/error.js
+++ middleware/error.js
@@ -7,11 +7,24 @@ const errorhandler = (err, req, res, next) => {
 
 	// Log to console for developer
 	console.log(err.stack.red);
+	console.log(err);
 
 	// Mongoose bad ObjectId
 	if (err.name === "CastError") {
 		const message = `Resource not found with id of ${err.value}`;
 		error = new ErrorResponse(message, 404);
+	}
+
+	// Mongoose duplicate key
+	if (err.code === 11000) {
+		const message = `Duplicate field value entered`;
+		error = new ErrorResponse(message, 400);
+	}
+
+	// Mongoose validation error
+	if (err.name === "ValidationError") {
+		const message = Object.values(err.errors).map((val) => val.message);
+		error = new ErrorResponse(message, 400);
 	}
 
 	res
