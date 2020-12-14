0c87149a8edf5f64a42393f45ab7953d1e8cc712
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-14 13:31:52 +0800

    Add login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,3 +20,32 @@ exports.register = asyncHandler(async (req, res, next) => {
 
 	res.status(201).json({ success: true, token });
 });
+
+// @desc    Login User
+// @route   POST /api/vi/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// Validate email and password
+	if (!email || !password) {
+		return next(new ErrorResponse("Please provide an email and password", 400));
+	}
+
+	const user = await User.findOne({ email }).select("+password");
+
+	if (!user) {
+		return next(new ErrorResponse("Invalid credentials", 401));
+	}
+
+	const isMatch = await user.matchPassword(password);
+
+	if (!isMatch) {
+		return next(new ErrorResponse("Invalid credentials", 401));
+	}
+
+	// create token
+	const token = user.getSignedJwtToken();
+
+	res.status(201).json({ success: true, token });
+});
