d4eef067582598ef839a303618eb9a577d558dd3
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-14 14:30:01 +0800

    Add role authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,9 +14,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
 		token = req.headers.authorization.split(" ")[1];
 	}
 	// else if (req.cookies.token) {
-  // 	token = req.cookies.token;
-  // }
-  
+	// 	token = req.cookies.token;
+	// }
+
 	if (!token) {
 		return next(new ErrorResponse("Not authorize to access this route", 401));
 	}
@@ -31,3 +31,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
 		next(new ErrorResponse("Not authorize to access this route", 401));
 	}
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+	return (req, res, next) => {
+		if (!roles.includes(req.user.role)) {
+			return next(
+				new ErrorResponse(
+					`User role ${req.user.role} is not authorized to access this route`,
+					403
+				)
+			);
+		}
+		next();
+	};
+};
