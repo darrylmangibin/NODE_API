df85b346009e543f45b9fb5b1b4e0339e4f9c69b
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-14 18:34:52 +0800

    Add review for a bootcamp functionality

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middleware/advancedResults");
 const Review = require("../models/Review");
-const { getReviews, getReview } = require("../controllers/reviews");
+const { getReviews, getReview, addReview } = require("../controllers/reviews");
 
 const { protect, authorize } = require("../middleware/auth");
 
@@ -11,7 +11,8 @@ router
 	.get(
 		advancedResults(Review, { path: "bootcamp", select: "name description" }),
 		getReviews
-	);
+	)
+	.post(protect, authorize("user", "admin"), addReview);
 
 router.route("/:id").get(getReview);
 
