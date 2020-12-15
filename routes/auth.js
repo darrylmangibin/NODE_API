cae1589a9dbafd095176f9965a435e388dcef786
Darryl Mangibin <darrylmangibin@gmail.com>
2020-12-15 08:44:42 +0800

    Add api security

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,8 @@ const {
 	forgotPassword,
 	resetPassword,
 	updateDatails,
-	updatePassword,
+  updatePassword,
+  logout
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -14,6 +15,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 router.get("/me", protect, getMe);
 router.put("/updatedetails", protect, updateDatails);
 router.put("/updatepassword", protect, updatePassword);
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -22,6 +28,29 @@ connectDB(db);
 
 // File Uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set sucurity headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 mins
+	max: 100,
+});
+
+app.use(limiter);
+
+// prevent http param polution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
